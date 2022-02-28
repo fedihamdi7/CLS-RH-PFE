b5dd82a06272a0fe00bb2b0d355c3f16f8288ea7
Fedi Hamdi <fedihamdi97@gmail.com>
2022-02-28 06:57:19 +0100

    table+pagination

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
+// Angular Material Modules
+import { MaterialModule } from './material/material.module';
 
 // Components
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-
-
-// Angular Material Modules
-import { MaterialModule } from './material/material.module';
 import { WorkComponent } from './employee/work/work.component';
 import { InternshipComponent } from './employee/internship/internship.component';
 
