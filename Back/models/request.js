82e7e9779aa975825b7c5586dfad66fc9910d56a
Amin-Bo <aminos.bo.12@gmail.com>
2022-03-02 00:09:41 +0100

    create user + login + add request

--- Back/models/request.js
+++ Back/models/request.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-
+const Schema = mongoose.Schema;
 const RequestSchema = new mongoose.Schema({
-    from: {type : String, required: false},
-    sent_date: {type : String, required: false},
-
-    
+    from: {type : Schema.Types.ObjectId, required: false},
+    sent_date: {type : Date, required: false,default:Date.now()},
+    done_date: {type : String, required: false },
+    type: {type : String,enum: ["work", "internship"], required: true},
+    status: {type :String ,enum: ["in progress", "done", "declined"], required: true , default: 'in progress'},
 });
 
 module.exports = mongoose.model('Request', RequestSchema);
