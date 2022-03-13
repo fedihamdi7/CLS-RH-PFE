326ef4d6dddc193c5d5f287c02570e2b2b6db3e5
Amin-Bo <aminos.bo.12@gmail.com>
2022-03-13 02:32:37 +0100

    fixed routes urls

--- Back/routes/contract.js
+++ Back/routes/contract.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const contractController = require('../controllers/contractController')
 router.post('/addContract',contractController.addContract)
-router.get('/',contractController.getAllContracts)
-router.get('/:id',contractController.getContractById)
+router.get('/getAllContarcts',contractController.getAllContracts)
+router.get('/getContractById:id',contractController.getContractById)
 router.get('/getContractBySupplierId/:id',contractController.getContractBySupplierId)
 module.exports =router;
