f7c6c7a66223d221f7ef26e74561b972ab289a7f
Fedi Hamdi <fedihamdi97@gmail.com>
2022-03-14 14:41:20 +0100

    better UX

--- Front/src/app/guards/is-not-auth.guard.ts
+++ Front/src/app/guards/is-not-auth.guard.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import {  CanActivate, Router } from '@angular/router';
 import { SharedService } from '../services/shared.service';
 
@@ -7,15 +6,12 @@ import { SharedService } from '../services/shared.service';
   providedIn: 'root'
 })
 export class IsNotAuthGuard implements CanActivate {
-  constructor ( private router : Router , private snackbar : MatSnackBar , private sharedService : SharedService){}
+  constructor ( private router : Router , private sharedService : SharedService){}
 
   canActivate(): boolean{
     // const user = JSON.parse(localStorage.getItem('user'));
     const user:any = this.sharedService.getUserFromLocalStorage();
     if (user) {
-      this.snackbar.open('You are already logged in', 'close', {
-        duration : 3000
-      });
       if (user.type == "admin") {
         this.router.navigate(['/admin']);
       }else if (user.type == "employee") {
