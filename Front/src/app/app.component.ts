bf1c3f03c297343ebb7e2822966b9a0d42692c97
Fedi Hamdi <fedihamdi97@gmail.com>
2022-03-25 00:40:37 +0100

    remove socket io

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { WebsocketService } from './services/websocket.service';
 import * as io from 'socket.io-client';
 
 @Component({
@@ -10,21 +9,10 @@ import * as io from 'socket.io-client';
 export class AppComponent implements OnInit{
   title = 'CLS';
   favIcon: HTMLLinkElement = document.querySelector('#appIcon');
-  constructor(private WebsocketService : WebsocketService) {
+  constructor() {
     this.favIcon.href = './../assets/logo-icon.png';
-    this.socket = io.connect('http://127.0.0.1:3000');
-
   }
-  private socket: any;
-  public data: any;
-
   ngOnInit(): void {
-    this.WebsocketService.listen("test event").subscribe((data) => {
-      // console.log(data)
-    })
-    this.socket.on('test event', data => {
-      this.data = data;
-      //console.log(data)
-    });
+
   }
 }
