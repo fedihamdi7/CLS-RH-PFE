b0c704e8d547809b3b6d50067cb7f03dfc86dec0
Fedi Hamdi <fedihamdi97@gmail.com>
2022-04-08 12:57:57 +0100

    updating the dashboard ui

--- Back/controllers/statsController.js
+++ Back/controllers/statsController.js
@@ -9,14 +9,12 @@ exports.getStats = (req, res) => {
     //get documents count
     User.countDocuments({}, (err, userCount) => {
         userCount = userCount-1;
-        Leave.countDocuments({}, (err, leaveCount) => {
             contract.countDocuments({}, (err, contractCount) => {
                 invoice.countDocuments({}, (err, invoiceCount) => {
                     request.countDocuments({}, (err, requestCount) => {
                         supplier.countDocuments({}, (err, supplierCount) => {
                             res.status(200).json({
                                 USERS : userCount,
-                                LEAVES : leaveCount,
                                 CONTRACTS : contractCount,
                                 INVOICES : invoiceCount,
                                 REQUESTS : requestCount,
@@ -26,6 +24,5 @@ exports.getStats = (req, res) => {
                     })
                 })
             })
-        })
     })
 }
